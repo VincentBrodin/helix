5aabb51cc6f0777975a2d242141a420381e4d37d
Vincent Brodin <vincent.brodin21@gmail.com>
2025-12-15 21:19:30 +0100

    Added flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.taplo
+        ];
+
+        shellHook = ''
+        '';
+      };
+    };
+}
